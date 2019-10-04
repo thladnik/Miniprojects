a9ecb2032776b23a2d562173244e3708ac4d4409
thladnik <tim.hladnik@gmail.com>
2019-10-04 15:41:13 +0200

    Added Icosphere; added horizontal stripe mask

--- Translation_Stimulus-Spherical_Arena.py
+++ Translation_Stimulus-Spherical_Arena.py
@@ -5,12 +5,112 @@
 
 import numpy as np
 from numpy import ndarray
+from scipy.spatial.transform import Rotation
 
 # Visualization
 from PyQt5 import QtCore, QtWidgets
 import pyqtgraph.opengl as gl
 
 import IPython
+
+gr = 1.61803398874989484820
+
+class IcosahedronSphere:
+
+    corners = [
+        [-1, gr, 0],
+        [1, gr, 0],
+        [-1, -gr, 0],
+        [1, -gr, 0],
+        [0, -1, gr],
+        [0, 1, gr],
+        [0, -1, -gr],
+        [0, 1, -gr],
+        [gr, 0, -1],
+        [ gr, 0, 1],
+        [-gr, 0, -1],
+        [-gr, 0, 1],
+    ]
+
+    faces = [
+
+        [0, 11, 5],
+        [0, 5, 1],
+        [0, 1, 7],
+        [0, 7, 10],
+        [0, 10, 11],
+
+        [3, 9, 4],
+        [3, 4, 2],
+        [3, 2, 6],
+        [3, 6, 8],
+        [3, 8, 9],
+
+        [1, 5, 9],
+        [5, 11, 4],
+        [11, 10, 2],
+        [10, 7, 6],
+        [7, 1, 8],
+
+        [4, 9, 5],
+        [2, 4, 11],
+        [6, 2, 10],
+        [8, 6, 7],
+        [9, 8, 1],
+    ]
+
+    cache = dict()
+
+    def __init__(self, subdiv_lvl):
+
+        # Calculate vertices
+        self.vertices = [self.vertex(*v) for v in self.corners]
+
+        # Subdivide faces
+        self.subdiv_lvl = subdiv_lvl
+        self.subdivide()
+
+    def vertex(self, x, y, z):
+        vlen = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+        return [i/vlen for i in (x, y, z)]
+
+    def middle_point(self, p1, p2):
+        key = '%i/%i' % (min(p1, p2), max(p1, p2))
+
+        if key in self.cache:
+            return self.cache[key]
+
+        v1 = self.vertices[p1]
+        v2 = self.vertices[p2]
+        middle = [sum(i)/2 for i in zip(v1, v2)]
+
+        self.vertices.append(self.vertex(*middle))
+        index = len(self.vertices) - 1
+
+        self.cache[key] = index
+
+        return index
+
+    def subdivide(self):
+        for i in range(self.subdiv_lvl):
+            subdiv = []
+            for face in self.faces:
+                v = [self.middle_point(face[0], face[1]),
+                     self.middle_point(face[1], face[2]),
+                     self.middle_point(face[2], face[0])]
+
+                subdiv.append([face[0], v[0], v[2]])
+                subdiv.append([face[1], v[1], v[0]])
+                subdiv.append([face[2], v[2], v[1]])
+                subdiv.append([v[0], v[1], v[2]])
+
+            self.faces = subdiv
+
+    def getVertices(self):
+        return np.array(self.vertices)
+
+    def getFaces(self):
+        return np.array(self.faces)
 
 def cart2sph(x, y, z):
     hxy = np.hypot(x, y)
@@ -143,16 +243,19 @@ def createHorizontalRectStripeMask(verts, theta_center, phi_center, phi_max):
     return mask
 
 def createHorizontalStripeMask(verts, theta_center, phi_center, phi_max):
+
+    raxis = np.array([1.0, 0.0, 0.0])
+    r = Rotation.from_rotvec(phi_center * raxis/np.linalg.norm(raxis))
+    verts = r.apply(verts)
+
     theta, phi, _ = cart2sph(verts[:,0], verts[:,1], verts[:,2])
 
     phi_baseline = -np.sin(theta) * phi_center
     phi_thresh = np.cos(theta-theta_center) * phi_max
 
     mask = np.zeros(verts.shape[0]).astype(bool)
-    #mask[(theta_center-np.pi/2 < theta) & (theta < theta_center+np.pi/2)
-    #     & (phi_center-phi_thresh < phi) & (phi < phi_center+phi_thresh)] = True
     mask[(theta_center - np.pi / 2 < theta) & (theta < theta_center + np.pi / 2)
-         & (phi_baseline-phi_thresh < phi) & (phi < phi_baseline+phi_thresh)] = True
+         & (-phi_thresh < phi) & (phi < +phi_thresh)] = True
     return mask
 
 
@@ -184,7 +287,7 @@ def createFragmentedTranslationStimulus(verts, whole_field, **masked_stimuli):
             mask = createHorizontalStripeMask(verts, 0., 0., np.pi/4)
 
         elif mask_type == 'horizontal_stripe':
-            mask = createHorizontalStripeMask(verts, -np.pi/2, np.pi/4, np.pi/8)
+            mask = createHorizontalStripeMask(verts, -np.pi/2, np.pi/4, np.pi/16)
             #IPython.embed()
 
         ## Complex masks
@@ -206,9 +309,20 @@ def createFragmentedTranslationStimulus(verts, whole_field, **masked_stimuli):
 
 if __name__ == '__main__':
 
-    # Create a sphere MeshData
-    md = gl.MeshData.sphere(rows=100, cols=200)
-    verts = md.vertexes()
+
+    use_iso = True
+    if use_iso:
+        print('Using ISO sphere')
+        sphere = IcosahedronSphere(6)
+        verts = sphere.getVertices()
+        md = gl.MeshData()
+        md.setVertexes(verts)
+        md.setFaces(sphere.getFaces())
+    else:
+        print('Using UV sphere')
+        # Create a sphere MeshData
+        md = gl.MeshData.sphere(rows=100, cols=200)
+        verts = md.vertexes()
 
     # Create custom pattern
     pattern = Pattern.Bars(sf=2.)
